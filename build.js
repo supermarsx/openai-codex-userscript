a19b6b3b09cdbfe8683e2766ee04807ee7de9d92
Mariana M <github_618a@imariana.com>
2025-07-16 15:52:04 +0100

    Build injects version from package.json

--- build.js
+++ build.js
@@ -2,8 +2,17 @@ const fs = require('fs');
 const path = require('path');
 const esbuild = require('esbuild');
 
+const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8'));
+const version = pkg.version;
+
+const indexTs = fs.readFileSync(path.join(__dirname, 'src', 'index.ts'), 'utf8').replace(/__VERSION__/g, version);
+
 esbuild.buildSync({
-  entryPoints: [path.join(__dirname, 'src', 'index.ts')],
+  stdin: {
+    contents: indexTs,
+    resolveDir: path.join(__dirname, 'src'),
+    sourcefile: 'index.ts',
+  },
   bundle: true,
   format: 'iife',
   platform: 'browser',
@@ -11,6 +20,6 @@ esbuild.buildSync({
   outfile: path.join(__dirname, 'dist', 'index.js'),
 });
 
-const header = fs.readFileSync(path.join(__dirname, 'src', 'header.js'), 'utf8');
+const header = fs.readFileSync(path.join(__dirname, 'src', 'header.js'), 'utf8').replace(/__VERSION__/g, version);
 const main = fs.readFileSync(path.join(__dirname, 'dist', 'index.js'), 'utf8');
 fs.writeFileSync(path.join(__dirname, 'openai-codex.user.js'), header + '\n' + main);
